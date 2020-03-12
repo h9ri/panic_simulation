b095dbb92cfd9a517fbbf922e102183971d81b3c
HARI KRISHNAN CK <ph1160562@iitd.ac.in>
2020-03-12 15:31:47 +0530

    Add files via upload

---
+++ display.py
@@ -0,0 +1,90 @@
+import Tkinter as tk
+import solver
+import random
+def _create_circle(self, x, y, r, **kwargs):
+    """Create a circle
+        
+    x the abscissa of centre
+    y the ordinate of centre
+    r the radius of circle
+    **kwargs optional arguments
+    return the drawing of a circle
+    """
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.create_circle = _create_circle
+
+def _coords_circle(self, target, x, y, r, **kwargs):
+    """Define a circle
+        
+    target the circle object
+    x the abscissa of centre
+    y the ordinate of centre
+    r the radius of circle
+    **kwargs optional arguments
+    return the circle drawing with updated coordinates
+    """
+    return self.coords(target, x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.coords_circle = _coords_circle
+
+class Display:
+    """Define the window used to display a simulation"""
+    
+    def __init__(self, balls, step, size):
+        """Initialize and launch the display"""
+        self.balls = balls
+        self.step = step
+        self.size = size
+        
+        self.window = tk.Tk()
+        self.canvas = tk.Canvas(self.window, width=self.size, height=self.size, bg="black")
+        self.canvas.pack()
+        self.canvas.focus_set()
+        self.drawing = self.create()
+        self.started = False
+    
+        start_button = tk.Button(self.window, text="Start", command=self.start)
+        stop_button = tk.Button(self.window, text="Pause", command=self.stop)
+        start_button.pack()
+        stop_button.pack()
+    
+        self.window.mainloop()
+    
+    def create(self):
+        """Create a drawing item for each solver.Ball object
+            
+        return a dictionary with solver.Ball objects as keys and their circle drawings as items
+        """
+        return {ball: self.canvas.create_circle(ball.position[0], ball.position[1], ball.radius, fill="white") for ball in self.balls}
+
+    def update(self):
+        """Update the drawing items for a time step"""
+        solver.solve_step_2(self.balls, self.step, self.size)
+        for ball in self.balls:
+            self.canvas.coords_circle(self.drawing[ball], ball.position[0], ball.position[1], ball.radius)
+        self.canvas.update()
+
+    def start(self):
+        """Start the animation"""
+        if not self.started:
+            self.started = True
+            self.animate()
+
+    def animate(self):
+        """Animate the drawing items"""
+        if self.started:
+            self.update()
+            self.window.after(0, self.animate)
+
+    def stop(self):
+        """Stop the animation"""
+        self.started = False
+
+# Test this module
+if __name__ == "__main__":
+    #balls = [solver.Ball(5., 5., [40.,40.], [5.,5.]), solver.Ball(5., 5., [480.,480.], [-15.,-15.]), solver.Ball(5., 5., [30.,470.], [10.,-10.])]
+    balls=[]
+    size = 500.
+    step = 0.1
+    for i in range(2):
+        balls.append(solver.Ball(80.,5,[float(random.randint(0,400)),float(random.randint(0,499))],0.5,size))    
+    Display(balls, step, size)
---
+++ solver.py
@@ -0,0 +1,92 @@
+import numpy as np
+import math
+class Ball:
+    """Define physics of elastic collision."""
+    
+    def __init__(self, mass, radius, position,velocity,size):
+        self.mass = mass
+        self.radius = radius
+        self.position = np.array(position)
+        self.fixed_velocity=self.compute_fixed_velocity(position,velocity,size)
+        self.velocity = np.array([0,0])
+        self.acceleration=np.array([0,0])
+        #self.vafter = np.copy(self.velocity) # temporary storage for velocity of next step
+
+    def compute_fixed_velocity(self,position,velocity,size):
+        position=self.position
+        if(position[1]<size/2):
+            theta=math.atan((size/2-position[1])/(size-position[0]))
+        else:
+            theta=-math.atan((position[1]-size/2)/(size-position[0]))
+        return np.array([math.cos(theta)*velocity,math.sin(theta)*velocity])
+    
+    def compute_wall_acceleration(self,step,toug,A,B,k1,k2,size):
+        position=self.position
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,position[0],np.array([1,0])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,size-position[1],np.array([0,1])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,size-position[0],np.array([-1,0])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,position[1],np.array([0,-1])))
+        
+
+    def compute_wall(self,toug,A,B,k1,k2,d,nij):
+        r,v,m=self.radius,self.velocity,self.mass
+        position=self.position
+        ans=np.array([0,0])
+               # print(ans)
+        if(r>=d):            
+            tij=np.array([-nij[1],nij[0]])
+            delta_v=np.dot(v,tij)
+            ans=np.add((k1/self.mass)*nij+(k2/self.mass)*delta_v*tij,ans,casting="unsafe")
+            #print(ans)
+        else:
+            ans=np.add(ans,A/m*np.exp((r-d)/B)*nij,casting="unsafe")
+
+        return ans
+
+
+
+    def compute_acceleration(self,ball2,toug,A,B,k1,k2,size):
+        self.fixed_velocity=self.compute_fixed_velocity(self.position,0.5,size)
+        self.acceleration+=(self.fixed_velocity-self.velocity)/toug
+        x1,x2=self.position,ball2.position
+        r1,r2=self.radius,ball2.radius
+        v1,v2=self.velocity,ball2.velocity
+        d=np.linalg.norm(x2-x1)
+        nij=(x2-x1)/d
+        
+        #print(self.acceleration)
+        if(r1+r2>=d):
+            tij=np.array([-nij[1],nij[0]])
+            delta_v=np.dot((v2-v1),tij)
+            self.acceleration+=k1/self.mass*nij+k2/self.mass*delta_v*tij
+            self.acceleration=np.add(A/self.mass*np.exp((r1+r2-d)/B)*nij,self.acceleration,casting="unsafe")
+            print(self.acceleration)
+            print("accele")
+        else:
+            self.acceleration=np.add(A/self.mass*np.exp((r1+r2-d)/B)*nij,self.acceleration,casting="unsafe")
+
+
+    def compute_step(self, step,size):
+        """Compute position of next step."""
+        #print(self.acceleration+"  "+self.position)
+        self.position = np.add(self.position,step * self.velocity+(1/2*self.acceleration*(step)*step)%size,casting="unsafe")
+        print(self.position)
+        print("ddgf")
+        self.velocity=np.add(self.velocity,step*self.acceleration)
+        
+
+
+def solve_step_2(ball_list,step,size):
+    A=2000
+    B=0.08
+    k1=120000
+    k2=240000
+    toug=0.5
+    for ball1 in ball_list:
+        ball1.acceleration=0
+        ball1.compute_wall_acceleration(step,toug,A,B,k1,k2,size)
+        for ball2 in ball_list:
+            if ball1 is not ball2:
+                ball1.compute_acceleration(ball2,toug,A,B,k1,k2,size)
+    for ball1 in ball_list:
+        ball1.compute_step(step,size)
