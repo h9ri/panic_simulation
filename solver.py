82e0b12d10d7aa6d5a1909fb08ed76fc28396483
HARI KRISHNAN CK <ph1160562@iitd.ac.in>
2020-03-19 13:14:55 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -22,24 +22,24 @@ class Ball:
     
     def compute_wall_acceleration(self,step,toug,A,B,k1,k2,size):
         position=self.position
-        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,position[0],np.array([1,0])))
-        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,size-position[1],np.array([0,1])))
-        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,size-position[0],np.array([-1,0])))
-        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,position[1],np.array([0,-1])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,position[0]%size,np.array([1,0])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,(size-position[1])%size,np.array([0,1])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,(size-position[0])%size,np.array([-1,0])))
+        self.acceleration=np.add(self.acceleration,self.compute_wall(toug,A,B,k1,k2,position[1]%size,np.array([0,-1])))
         
 
     def compute_wall(self,toug,A,B,k1,k2,d,nij):
         r,v,m=self.radius,self.velocity,self.mass
         position=self.position
         ans=np.array([0,0])
-        if(r>d):            
+        if(r>=d):            
             tij=np.array([-nij[1],nij[0]])
             delta_v=np.dot(v,tij)
-            ans=np.add((k1/self.mass)*nij+(k2/self.mass)*delta_v*tij,ans,casting="unsafe")
+            ans=np.add((k1/m)*nij-(k2/m)*delta_v*tij,ans,casting="unsafe")
             ans=np.add(ans,A/m*np.exp((r-d)/B)*nij,casting="unsafe")
+            
         else:
             ans=np.add(ans,A/m*np.exp((r-d)/B)*nij,casting="unsafe")
-
         return ans
 
 
@@ -50,11 +50,11 @@ class Ball:
         x1,x2=self.position,ball2.position
         r1,r2=self.radius,ball2.radius
         v1,v2=self.velocity,ball2.velocity
-        d=np.linalg.norm(x2-x1)
-        nij=(x1-x2)/d
-        if(r1+r2>=d):
+        d=np.linalg.norm(x1-x2)
+        nij=(x2-x1)/d
+        if(r1+r2>d):
             tij=np.array([-nij[1],nij[0]])
-            delta_v=np.dot((v2-v1),tij)
+            delta_v=np.dot((v1-v2),tij)
             self.acceleration+=k1/self.mass*nij+k2/self.mass*delta_v*tij
             self.acceleration=np.add(A/self.mass*np.exp((r1+r2-d)/B)*nij,self.acceleration,casting="unsafe")
         else:
@@ -64,7 +64,10 @@ class Ball:
     def compute_step(self, step,size):
         """Compute position of next step."""
         #print(self.acceleration+"  "+self.position)
-        self.position = np.add(self.position,step * self.velocity+(1/2*self.acceleration*(step)*step)%size,casting="unsafe")
+        self.position = np.add(self.position,((step * self.velocity)+(1/2*self.acceleration*step*step)),casting="unsafe")
+        #print(self.position)
+        #self.position[0]=self.position[0]%size
+        #self.position[1]=self.position[1]%size
         self.velocity=np.add(self.velocity,step*self.acceleration)
         
 
@@ -77,9 +80,9 @@ def solve_step_2(ball_list,step,size):
     toug=0.5
     for ball1 in ball_list:
         ball1.acceleration=0
-        ball1.compute_wall_acceleration(step,toug,A,B,k1,k2,size)
         for ball2 in ball_list:
             if ball1 is not ball2:
                 ball1.compute_acceleration(ball2,toug,A,B,k1,k2,size)
+        ball1.compute_wall_acceleration(step,toug,A,B,k1,k2,size)
     for ball1 in ball_list:
         ball1.compute_step(step,size)
