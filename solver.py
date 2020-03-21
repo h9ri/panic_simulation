ef440e34d9d78bec31c72b3d1c7f74b75b596477
HARI KRISHNAN CK <ph1160562@iitd.ac.in>
2020-03-21 10:17:38 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,24 +1,32 @@
 import numpy as np
 import math
+import random
 class Ball:
-    """Define physics of elastic collision."""
     
     def __init__(self, mass, radius, position,velocity,size):
         self.mass = mass
         self.radius = radius
         self.position = np.array(position)
-        self.fixed_velocity=self.compute_fixed_velocity(position,velocity,size)
+        self.fixed_velocity=np.array([0,0])
         self.velocity = np.array([0,0])
         self.acceleration=np.array([0,0])
-        #self.vafter = np.copy(self.velocity) # temporary storage for velocity of next step
+        self.random_point=np.array([0,0])
+        self.fdr=False  # check if its inside the circle 
 
     def compute_fixed_velocity(self,position,velocity,size):
         position=self.position
-        if(position[1]<size/2):
-            theta=math.atan((size/2-position[1])/(size-position[0]))
-        else:
-            theta=-math.atan((position[1]-size/2)/(size-position[0]))
-        return np.array([math.cos(theta)*velocity,math.sin(theta)*velocity])
+        if not(self.fdr):#checking the direction of velocity to be assigned 
+            if(position[1]<size/2):
+                theta=math.atan((size/2-position[1])/(size-position[0]))
+            else:
+                theta=-math.atan((position[1]-size/2)/(size-position[0]))
+            return np.array([math.cos(theta)*velocity,math.sin(theta)*velocity])
+        else:    # code for velocity calculation of return 
+            random_position=self.random_point[1]
+            theta=math.atan((position[1]-random_position)/position[0])
+            return np.array([-math.cos(theta)*velocity,-math.sin(theta)*velocity])# negative because direction is reversed 
+
+
     
     def compute_wall_acceleration(self,step,toug,A,B,k1,k2,size):
         position=self.position
@@ -35,7 +43,7 @@ class Ball:
         if(r>=d):            
             tij=np.array([-nij[1],nij[0]])
             delta_v=np.dot(v,tij)
-            ans=np.add((k1/m)*nij-(k2/m)*delta_v*tij,ans,casting="unsafe")
+            ans=np.add((k1/m)*nij+(k2/m)*delta_v*tij,ans,casting="unsafe")
             ans=np.add(ans,A/m*np.exp((r-d)/B)*nij,casting="unsafe")
             
         else:
@@ -51,10 +59,10 @@ class Ball:
         r1,r2=self.radius,ball2.radius
         v1,v2=self.velocity,ball2.velocity
         d=np.linalg.norm(x1-x2)
-        nij=(x2-x1)/d
+        nij=(x1-x2)/d
         if(r1+r2>d):
             tij=np.array([-nij[1],nij[0]])
-            delta_v=np.dot((v1-v2),tij)
+            delta_v=np.dot((v2-v1),tij)
             self.acceleration+=k1/self.mass*nij+k2/self.mass*delta_v*tij
             self.acceleration=np.add(A/self.mass*np.exp((r1+r2-d)/B)*nij,self.acceleration,casting="unsafe")
         else:
@@ -63,14 +71,16 @@ class Ball:
 
     def compute_step(self, step,size):
         """Compute position of next step."""
-        #print(self.acceleration+"  "+self.position)
         self.position = np.add(self.position,((step * self.velocity)+(1/2*self.acceleration*step*step)),casting="unsafe")
-        #print(self.position)
-        #self.position[0]=self.position[0]%size
-        #self.position[1]=self.position[1]%size
         self.velocity=np.add(self.velocity,step*self.acceleration)
         
-
+    def  check_inside_fdr(self,size,radius):
+        position=self.position
+        po=np.array([size,size/2])
+        if np.linalg.norm(position-po) < radius and not self.fdr:
+            self.fdr=True
+            self.random_point[1]+=random.randint(0,499)# assigning a random point on the 1st wall 
+            print(self.random_point)#printing the position they are assigned to go
 
 def solve_step_2(ball_list,step,size):
     A=2000
@@ -80,6 +90,7 @@ def solve_step_2(ball_list,step,size):
     toug=0.5
     for ball1 in ball_list:
         ball1.acceleration=0
+        ball1.check_inside_fdr(size,20) # checking if ball inside the radius
         for ball2 in ball_list:
             if ball1 is not ball2:
                 ball1.compute_acceleration(ball2,toug,A,B,k1,k2,size)
